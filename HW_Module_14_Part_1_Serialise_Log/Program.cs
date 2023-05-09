b17ca6b166ce63ff749ee1860caf5bac960951e0
MatvX_lm73 <MatvX_lm73@student.itstep.org>
2023-05-10 01:34:03 +0300

    Добавьте файлы проекта.

---
+++ HW_Module_14_Part_1_Serialise_Log/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+
+namespace HW_Module_14_Part_1_Serializer_Log
+{
+    /*Создайте программу для работы с массивом дробей (числитель
+и знаменатель). Она должна иметь такую функциональность:
+1. Ввод массива дробей с клавиатуры
+2. Сериализация массива дробей
+3. Сохранение сериализованного массива в файл
+4. Загрузка сериализованного массива из файла. После
+загрузки нужно произвести десериализацию
+Выбор конкретного формата сериализации необходимо сделать
+вам. Обращаем ваше внимание, что выбор должен быть
+обоснованным.
+*/
+    internal class Program
+    {
+        [Serializable]
+
+        
+        public  class Fraction
+        {
+            [DataMember]
+            public int x { set; get; }
+            [DataMember]
+            public int y { set; get; }
+            public Fraction() { }
+            public override string ToString()
+            {
+                return x.ToString() + " / " + y.ToString();
+            }
+            public void Output()
+            {
+                Console.WriteLine(x + " / " + y);
+            }
+            public void SetFraction()
+            {
+                Console.WriteLine("Enter x : ");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter y : ");
+                y = int.Parse(Console.ReadLine());
+
+            }
+        }
+        [Serializable]
+       
+        public class FractionArr
+        {
+            public List<Fraction> fractionList = new List<Fraction>();
+            public FractionArr() { }
+            public FractionArr(List<Fraction> list) =>fractionList.AddRange(list);
+            public void Add(Fraction frac) => fractionList.Add(frac);
+            public void Add()
+            {
+                Fraction fraction = new Fraction();
+                fraction.SetFraction();
+                fractionList.Add(fraction);
+
+            }
+            public void Output()
+            {
+                foreach (var frac in fractionList) frac.Output();
+            }
+        }
+
+
+         class Serialiser
+        {
+            
+            
+            public void SerializeJSON(FractionArr obj)
+            {
+                Stream stream = new  FileStream("Fraction.json", FileMode.Create);
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(FractionArr));
+                jsonFormatter.WriteObject(stream, obj);
+                stream.Close();
+                Console.WriteLine("Json serializer OK");
+            }
+            public FractionArr DeSerializerJSON(string path = "Fraction.json")
+            {
+                Stream stream = new FileStream(path, FileMode.Open);
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer (typeof(FractionArr));
+                FractionArr fractionArr = (FractionArr)jsonFormatter.ReadObject(stream);
+                stream.Close ();
+                Console.WriteLine("Deserialize JSON Ok");
+                return fractionArr;
+
+            }
+
+            public void SerializeXml(FractionArr obj)
+            {
+                Stream stream = new FileStream("Fraction.xml" , FileMode.Create);
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(FractionArr));
+                xmlSerializer.Serialize(stream, obj);
+                stream.Close();
+                Console.WriteLine("Sml Serialize OK");
+            }
+
+            public FractionArr DeSerializerXml(string path = "Fraction.xml")
+            {
+                Stream stream = new FileStream(path, FileMode.Open);
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(FractionArr));
+                FractionArr Res = (FractionArr)xmlSerializer.Deserialize(stream);
+                Console.WriteLine("Deserialize XML Ok");
+
+                return Res;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            FractionArr obj = new FractionArr();
+            obj.Add();
+            obj.Add();
+            obj.Add();
+            Serialiser serialiser = new Serialiser();
+            serialiser.SerializeJSON(obj);
+            FractionArr objRes = serialiser.DeSerializerJSON();
+            objRes.Output();
+            //serialiser.SerializeXml(obj);         //НА ЭТОМ ЭТАПЕ ВЫЛЕТАЕТ ОШИБКА СВЯЗАННАЯ С общЕДОСТУПНОСТЬЮ К ТИПУ. 
+            //FractionArr objRes2 = serialiser.DeSerializerXml();
+            //objRes2.Output();
+
+        }
+    }
+}
---
+++ Task_2_log/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+/*Создайте программу для работы с информацией о журнале.
+Нужно хранить такую информацию об журнале:
+1. Название журнала
+2. Название издательства
+3. Дата выпуска
+4. Количество страниц
+У программы должна быть такая функциональность:
+1. Ввод информации о журнале
+2. Вывод информации о журнале
+3. Сериализация журнала
+4. Сохранение сериализованного журнала в файл
+5. Загрузка сериализованного журнала из файла. После
+загрузки нужно произвести десериализацию журнала.
+Выбор конкретного формата сериализации необходимо сделать
+вам. Обращаем ваше внимание, что выбор должен быть
+обоснованным.
+*/
+/*Добавьте к предыдущему заданию список статей из журнала.
+Нужно хранить такую информацию о каждой статье:
+1. Название статьи
+2. Количество символов
+3. Анонс статьи
+Измените функциональность из предыдущего задания таким
+образом, чтобы она учитывала список статей.
+Выбор конкретного формат сериализации необходимо сделать
+вам. Обращаем ваше внимание, что выбор должен быть
+обоснованным.*/
+/*Добавьте к предыдущему заданию возможность создания
+массива журналов.
+Измените функциональность из второго задания таким образом,
+чтобы она учитывала массив журналов.
+Выбор конкретного формат сериализации необходимо сделать
+вам. Обращаем ваше внимание, что выбор должен быть
+обоснованным.*/
+
+
+
+
+namespace Task_2_log
+{
+    [Serializable]
+    
+    
+    public class Journal
+    {
+       public List<Article> articles=new List<Article>(); 
+        public string Name { get; set; }
+        public string PublishHouse { get; set; }
+        public int Year { get; set; }
+        public int AmountPages { get; set; }
+        public int Count;
+        public Journal() { }
+        public Journal(string name, string publishHouse, int year, int AmountPages)
+        {
+            this.Name = name;
+            this.PublishHouse = publishHouse;
+            this.Year = year;
+            this.AmountPages = AmountPages;
+        }
+        public void OutputInfo()
+        {
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Publish House : " + PublishHouse);
+            Console.WriteLine("Year : " + Year);
+            Console.WriteLine("Amount Pages : " + AmountPages);
+            foreach (var article in articles) 
+            {
+                    article.OutputInfo();
+                    Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public void SetInfo()
+        {
+            Console.WriteLine("Enter info about Magazine");
+            Console.WriteLine("Name : ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Year : ");
+            Year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Publish House : ");
+            PublishHouse = Console.ReadLine();
+            Console.WriteLine("Amount pages : ");
+            AmountPages = int.Parse(Console.ReadLine());
+        }
+        public void SetArticles(List<Article> articles)
+        {
+            
+            this.articles.AddRange(articles);
+            Count += articles.Count;
+        }
+    }
+
+
+    [Serializable]
+    public class Article
+    {
+        public string NameArticle { get; set; }
+        public int AmountLetters { get; set; }
+        public int Preview { get; set; }
+
+
+        public Article() { }
+        public Article(string NameArticle, int AmountLetters, int Preview)
+        {
+            this.NameArticle = NameArticle;
+            this.AmountLetters = AmountLetters;
+            this.Preview = Preview;
+        }
+        public void OutputInfo()
+        {
+        Console.WriteLine("\tName Article : " + NameArticle);
+        Console.WriteLine("\tAmount letters : " + AmountLetters);
+        Console.WriteLine("\tPreview : " +  Preview);
+        }
+    }
+    class Serialiser
+    {
+
+        public void SerializeXml(Journal obj)
+        {
+            Stream stream = new FileStream("Journal.xml", FileMode.Create);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Journal));
+            xmlSerializer.Serialize(stream, obj);
+            stream.Close();
+            Console.WriteLine("xml Serialize OK");
+        }
+
+        public Journal DeSerializerXml(string path = "Journal.xml")
+        {
+            Stream stream = new FileStream(path, FileMode.Open);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Journal));
+            Journal Res = (Journal)xmlSerializer.Deserialize(stream);
+            Console.WriteLine("Deserialize XML Ok");
+
+            return Res;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Start. Initialising Journal with articles\n\n");
+            
+                List<Article> articles = new List<Article>();
+            articles.Add(new Article("Статья 1", 700, 2011));
+            articles.Add(new Article("Статья 2", 200, 2017));
+            articles.Add(new Article("Статья 3", 500, 2012));
+            articles.Add(new Article("Статья 4", 800, 2015));
+            articles.Add(new Article("Статья 5", 100, 2014));
+
+            Console.WriteLine("Now we saving data to File   Journal.xml\n\n");
+            Journal journal = new Journal("Журнал", "Какое-то издательство", 2030, 380);
+            journal.SetArticles(articles);
+            journal.OutputInfo();
+            Console.WriteLine("Count = " + journal.Count);
+
+
+            Serialiser serialiser = new Serialiser();
+
+            Console.WriteLine("Now we download data from File   Journal.xml\n\n");
+            serialiser.SerializeXml(journal);
+            Journal journal2 = serialiser.DeSerializerXml();
+            journal2.OutputInfo();
+            Console.WriteLine("Count = " + journal2.Count);
+
+
+        }
+    }
+}
